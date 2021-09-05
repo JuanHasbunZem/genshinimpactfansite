2dd29f2f25052b624144b024c921ef583899eaa1
JuanHasbunZem <zemenarwow@gmail.com>
2021-09-05 08:37:45 -0700

    update README, update template

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function App(){
+  return (
+    <React.Fragment>
+      <Header />
+      <Sidebar />
+      <MainScreen />
+    </React.Fragment>
+  );
+}
+
+export default App;
